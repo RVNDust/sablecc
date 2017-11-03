cb9881df630b12c9692547def6370dadabc714a7
Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>
2017-11-03 14:20:32 -0400

    Add escape for ' in text parts
    Signed-off-by: Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>

--- src/org/sablecc/objectmacro/launcher/ObjectMacro.java
+++ src/org/sablecc/objectmacro/launcher/ObjectMacro.java
@@ -948,7 +948,7 @@ public class ObjectMacro {
             if(stringPart instanceof ATextStringPart){
                 String stringPartText = ((ATextStringPart) stringPart).getText().getText();
 
-                stringPartText = stringPartText.replaceAll("'", "\\'");
+                stringPartText = stringPartText.replaceAll("'", "\\\\'");
 
                 mTextArgument.newStringPart(stringPartText);
             }
@@ -1004,7 +1004,7 @@ public class ObjectMacro {
             if(stringPart instanceof ATextStringPart){
                 String stringPartText = ((ATextStringPart) stringPart).getText().getText();
 
-                stringPartText = stringPartText.replaceAll("'", "\\'");
+                stringPartText = stringPartText.replaceAll("'", "\\\\'");
 
                 mDirective.newStringPart(stringPartText);
             }
