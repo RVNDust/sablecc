101c29a46bbe1d447c3aeab6d6e384961a9c8504
Yvan Denais <rvndust@hotmail.fr>
2018-05-16 15:46:00 -0400

    Prepare some examples using macro in c.

---
+++ tests/objectmacro/c/LinkedList.c
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#include<LinkedListNode.h>
+#include<LinkedList.h>
+
+int main()
+{
+
+    LinkedList *linkedList = new_LinkedList();
+
+    linkedList->vt->push(linkedList, 1, sizeof(int));
+    linkedList->vt->push(linkedList, 5, sizeof(int));
+    linkedList->vt->push(linkedList, 10, sizeof(int));
+
+    Node *currentNode = linkedList->nodes;
+
+    if(currentNode != NULL)
+    {
+        printf(currentNode->data);
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            printf(currentNode->data);
+        }
+    }
+
+    #ifdef __WIN32__
+        system("PAUSE");
+    #endif
+        return 0;
+}
